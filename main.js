44afe69907e4a4b93e0002f6fd9927c848471146
han18 <han.fadah@gmail.com>
2024-10-12 22:46:29 -0400

    created the style

---
+++ main.js
@@ -0,0 +1,27 @@
+// Create your game here!
+
+const appBody = document.getElementById("app");
+
+// appending a p element to the app div
+const paragraph = app.appendChild(document.createElement("p"));
+paragraph.style.color = "blue";
+
+// displaying a message text
+paragraph.textContent = "This is a guessing game";
+
+// guessing a number through a prompt
+let guessNumber = Number(prompt("choose a number from 1 to 5"));
+
+// creating a random number to be guessed
+let secretNumber = Math.trunc(Math.random() * 5) + 1;
+
+// checking the condition of the number prompt input
+if (guessNumber === secretNumber) {
+  document.querySelector(
+    ".message"
+  ).textContent = `YAY! You got it! The secret number is ${secretNumber}`;
+} else if (guessNumber !== secretNumber) {
+  console.log((document.querySelector(".message").textContent = `Try again`));
+} else {
+  alert("Do it all over again");
+}
